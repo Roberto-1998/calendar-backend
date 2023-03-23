f43585e70d5f06cfc0f91099f8edc630611bb891
Roberto <rcastellong98@gmail.com>
2023-03-23 22:57:02 +0100

    [add] react static files

--- index.js
+++ index.js
@@ -13,18 +13,18 @@ dbConnection();
 /* CORS */
 app.use(cors());
 
-/* Directorio publico */
-if (process.env.NODE_ENV == 'production') {
-  app.use(express.static(path.join(__dirname, 'public')));
-  app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'public', 'index.html')));
-}
-
 /* Lectura y parseo del body */
 app.use(express.json());
 
 /* Rutas */
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/events', require('./routes/events'));
+
+/* Directorio publico */
+if (process.env.NODE_ENV == 'production') {
+  app.use(express.static(path.join(__dirname, 'public')));
+  app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'public', 'index.html')));
+}
 
 /* Escuchar peticiones */
 app.listen(process.env.PORT, () => {
